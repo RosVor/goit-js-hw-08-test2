5dbe30293c980ae3ba881c064c5db9d256dbd7cd
RosVor <ros.vorokh@gmail.com>
2023-07-09 23:56:05 +0300

    test 1 a 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+const currentTimeKey = 'videoplayer-current-time';
+player.on('timeupdate', _.throttle(function (event) {
+  const currentTime = event.seconds;
+  localStorage.setItem(currentTimeKey, currentTime);
+}, 1000));
+document.addEventListener('DOMContentLoaded', function () {
+  const savedTime = localStorage.getItem(currentTimeKey);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
